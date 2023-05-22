b153cd23aed616ba71f4a7333bbe73e94f3c7049
“[firstname <hutapeniko@gmail.com>
2023-05-22 16:42:16 +0700

    update middleware

--- src/routes/user-route/user_route.js
+++ src/routes/user-route/user_route.js
@@ -7,11 +7,11 @@ const updateModule = require("../../controllers/handle-user/profile_handler.js")
 route.post("/register", myModule.handlerRegister);
 route.post("/login", myModule.handlerLogin);
 route.delete("/logout", verifyToken, myModule.handlerLogout);
-route.post("/profile/email", updateModule.updateEmail);
-route.post("/profile/telepon", updateModule.updateTelepon);
-route.post("/profile/username", updateModule.updateUsername);
-route.post("/profile/upload", updateModule.updateProfile);
-route.delete("/profile/delete", updateModule.deleteProfile);
+route.post("/profile/email", verifyToken, updateModule.updateEmail);
+route.post("/profile/telepon", verifyToken, updateModule.updateTelepon);
+route.post("/profile/username", verifyToken, updateModule.updateUsername);
+route.post("/profile/upload", verifyToken, updateModule.updateProfile);
+route.delete("/profile/delete", verifyToken, updateModule.deleteProfile);
 route.get("/testing", verifyToken, (req, res) => {
   res.send("look at me");
 });
