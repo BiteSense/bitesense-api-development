b54918169638999239eed5bab2f32ed2bd5121ea
“[firstname <hutapeniko@gmail.com>
2023-05-26 22:06:45 +0700

    create service user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const db = require("../configs/db.configs");
 const jwt = require("jsonwebtoken");
+const service = require("../services/user.services");
 
 const handlerRegister = async (req, res) => {
   const { username, email, password, repassword } = req.body;
@@ -11,28 +12,19 @@ const handlerRegister = async (req, res) => {
       message: "Password and Repassword different",
     });
   }
-
   //Checking Already email
-  const emailExicst = `SELECT email from users WHERE email = '${email}'`;
-  const exicst = await db.query(emailExicst);
-  if (exicst[0][0]) {
+  if ((await service.checkEmail(email)) === false) {
     return res.json({
       status: "error",
       message: "Email Already Exicst",
     });
   }
-
   //Enkrip Password
   const salt = await bcrypt.genSalt();
   const hashPassword = await bcrypt.hash(password, salt);
 
-  //Create Id
-  const id_user = Math.floor(Math.random() * 1000000000 + 1);
-  // Default foto
-  const foto_user = "https://storage.googleapis.com/staging_product/default-profile.jpg";
   try {
-    const sql = `INSERT INTO users (id_user, username, email, password, token, no_telepon, foto_user) VALUES ('${id_user}', '${username}', '${email}', '${hashPassword}', NULL, NULL, '${foto_user}')`;
-    db.query(sql);
+    service.registerUser(username, email, hashPassword);
     return res.status(200).json({
       code: 200,
       status: "success",
@@ -45,16 +37,13 @@ const handlerLogin = async (req, res) => {
   const { email, password } = req.body;
   try {
     // Check Email
-    const emailExicst = `SELECT * FROM users WHERE email = '${email}'`;
-    const exisct = await db.query(emailExicst);
-    // Checking data in database which given by email parser from body
-    if (!exisct[0][0]) {
+    if (await service.checkEmail(email)) {
       return res.json({
         status: "error",
         message: "Email belum pernah didaftarkan",
       });
     }
-    const data = exisct[0][0];
+    const data = await service.getAllByEmail(email);
     const match = await bcrypt.compare(password, data.password);
 
     if (!match) {
@@ -69,9 +58,7 @@ const handlerLogin = async (req, res) => {
       expiresIn: "1d",
     });
 
-    const sql = `UPDATE users SET token = '${token}' WHERE id_user = '${userid}'`;
-    const result = await db.query(sql);
-    if (!result) {
+    if (!(await service.updateUserToken(token, userid))) {
       return res.json({
         status: "error",
       });
@@ -96,20 +83,13 @@ const handlerLogout = async (req, res) => {
         message: "Unauthorize",
       });
 
-    const sql = `SELECT token FROM users WHERE token = '${token}'`;
-    const result = await db.query(sql);
-
-    const data = result[0][0];
+    const data = await service.getAllByToken;
     if (!data)
       return res.status(204).json({
         status: "error",
         message: "Unauthorize",
       });
-
-    const sql1 = `UPDATE users SET token = NULL WHERE id_user = '${data.id_user}'`;
-    const result1 = await db.query(sql1);
-
-    if (result1[0][0])
+    if (!service.clearToken(data.id_user))
       return res.json({
         status: "error",
         message: "Gagal Logout",
